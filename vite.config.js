b91904ae75697de4157d7a8e50ff572bf3ea05b7
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-04-15 21:52:59 +0900

    hotfix: downgrade crxjs

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,32 @@ import vue from '@vitejs/plugin-vue'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json' assert { type: 'json' }
 
+const viteManifestHackIssue846 = {
+  // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
+  name: 'manifestHackIssue846',
+  renderCrxManifest(_manifest, bundle) {
+    bundle['manifest.json'] = bundle['.vite/manifest.json']
+    bundle['manifest.json'].fileName = 'manifest.json'
+    delete bundle['.vite/manifest.json']
+  },
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), crx({ manifest })],
+  plugins: [vue(), viteManifestHackIssue846, crx({ manifest })],
   build: {
     rollupOptions: {
       input: {
         popup: 'src/entrypoints/popup/index.html',
         content: 'src/entrypoints/content/index.ts',
       },
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
     },
   },
 })
